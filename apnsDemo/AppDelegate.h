05ea8f1cf36b8fb2e8f2f20ce363da71a1580585
zhengjing zhu <zhuzhengjing@gmail.com>
2015-06-30 13:50:33 +0800

    Initial Commit

---
+++ apnsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  apnsDemo
+//
+//  Created by 朱正晶 on 15/6/30.
+//  Copyright (c) 2015年 chisalsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
